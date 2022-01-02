1f2c358aa855acec2a38e5c01e4e04ff0a44d553
“om2122” <omct2122@gmail.com>
2022-01-03 00:38:36 +0530

    Reverse list

---
+++ src/main/java/com/example/string/ReverseString.java
@@ -0,0 +1,24 @@
+package com.example.string;
+
+public class ReverseString {
+
+	public static String reverse(String input) {
+
+		if (input == null) {
+			return input;
+		}
+
+		String output = "";
+
+		for (int i = input.length() - 1; i >= 0; i--) {
+			output = output + input.charAt(i);
+		}
+
+		return output;
+	}
+
+	public static void main(String[] args) {
+		String st = "Hi Om";
+		System.out.println("Reverse :: "+reverse(st));
+	}
+}
