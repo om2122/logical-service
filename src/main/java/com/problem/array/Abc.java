e4209f81c4dd510a5289e3aafc6ff31aa4393c2f
“om2122” <omct2122@gmail.com>
2022-02-10 13:30:28 +0530

    commit

--- src/main/java/com/problem/array/Abc.java
+++ src/main/java/com/problem/array/Abc.java
@@ -4,4 +4,5 @@ public class Abc {
 	public static void main(String[] args) {
 		System.out.println("Hi");
 	}
+	
 }
--- src/main/java/com/problem/sorting/Abc.java
+++ src/main/java/com/problem/sorting/Abc.java
@@ -1,7 +1,9 @@
 package com.problem.sorting;
 
 public class Abc {
+	
 	public static void main(String[] args) {
 		System.out.println("Hi");
 	}
+
 }
